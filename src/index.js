a49c9d91be2835fb2650d52abe490bdb08dce343
ThinhVu <vutrongthinhk7@gmail.com>
2023-03-31 17:50:04 +0700

    fix: correct try/catch usage

--- src/index.js
+++ src/index.js
@@ -2,19 +2,30 @@ const {routeMetadataCollectorFactory} = require('routerex')
 const generateHtml = require('./generator/html')
 const generatePostman = require('./generator/postman')
 
-module.exports = function(config) {
-  if (!config)
+module.exports = function (config) {
+  if (!config) {
     throw new Error('No config provided')
+  }
   const {onHtmlGenerated, onPostmanGenerated, onError} = config
   return routeMetadataCollectorFactory({
     onMetadataGathered: async metadatas => {
-      try {
-        if (onHtmlGenerated)
-          setImmediate(() => generateHtml(metadatas, onHtmlGenerated))
-        if (onPostmanGenerated)
-          setImmediate(() => generatePostman(metadatas, onPostmanGenerated))
-      } catch (e) {
-        onError && onError(e)
+      if (onHtmlGenerated) {
+        setImmediate(() => {
+          try {
+            generateHtml(metadatas, onHtmlGenerated)
+          } catch (e) {
+            onError && onError(e)
+          }
+        })
+      }
+      if (onPostmanGenerated) {
+        setImmediate(() => {
+          try {
+            generatePostman(metadatas, onPostmanGenerated)
+          } catch (e) {
+            onError && onError(e)
+          }
+        })
       }
     }
   })
